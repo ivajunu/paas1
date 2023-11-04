066baa32400355b943e6e2505d46d6f44b852aa9
ivajunu <ivanna.junuzovic@live.se>
2023-11-04 18:01:07 +0100

    hello

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 var cors = require("cors");
+path = require("path");
 
 const app = express(),
   port = process.env.PORT || 3000;
 
-app.get("/", (_request, response) => {
+app.get("/api", (_request, response) => {
   response.send({ ivanna: "e bäst" });
 });
+
+app.use(express.static(path.join(path.resolve(), "dist")));
 
 app.listen(port, () => {
   console.log(`Redo på http://localhost:${port}/`);
 });
-
-app.use(express.static(path.join(path.resolve(), "dist")));
 
 app.listen(80, function () {
   console.log("CORS-enabled web server listening on port 80");
